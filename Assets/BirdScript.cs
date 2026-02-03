a0af1e86a684d918eeb183a6be7dad2dc7b99fd8
MD ASIF IQBAL <81494536+AceAsif@users.noreply.github.com>
2026-02-03 22:22:51 +1100

    Initial Unity project import and bird script

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This replaces the old Input.GetKeyDown
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrength;
+        }
+        
+    }
+}
