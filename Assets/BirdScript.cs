30289f711a6d5589749d5f342c3dfa496624f5f1
MD ASIF IQBAL <81494536+AceAsif@users.noreply.github.com>
2026-02-04 22:12:23 +1100

    Added the bird collision to the pipe to end game.

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,21 +7,29 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // This replaces the old Input.GetKeyDown
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && birdIsAlive == true)
         {
             myRigidbody.linearVelocity = Vector2.up * flapStrength;
         }
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
